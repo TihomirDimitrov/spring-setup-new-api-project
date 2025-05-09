bb596fc0e198809b64a05bc0794dab3b0e39390b
TihomirDimitrov <tihomirdimitrov@coherentsolutions.com>
2025-05-09 13:04:31 +0300

    Initial comit

---
+++ spring-setup-ownproject/src/test/java/com/spring/api/example/spring_setup_ownproject/SpringSetupOwnprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.api.example.spring_setup_ownproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSetupOwnprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
